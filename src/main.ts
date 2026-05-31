6ae6ef3dbbb7a7294ef85476cba24d6d6b50818e
Sebastián Rodulfo <csrodulfo@outlook.com>
2026-05-31 20:20:38 -0300

    KickOff

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import type { NestExpressApplication } from '@nestjs/platform-express';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
